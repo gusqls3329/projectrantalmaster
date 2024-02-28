4a7d706d213eb4da3f697b19a3473012d32b9d28
devYule <privateDevYule@gmail.com>
2024-02-28 10:32:42 +0900

    2024-02-29 --by Hyunmin

--- src/main/java/com/team5/projrental/administration/enums/DisputeKind.java
+++ src/main/java/com/team5/projrental/administration/enums/DisputeKind.java
@@ -1,4 +1,24 @@
 package com.team5.projrental.administration.enums;
+
+import com.team5.projrental.common.exception.ErrorCode;
+import com.team5.projrental.common.exception.thrid.ClientException;
+import com.team5.projrental.entities.enums.DisputeReason;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
 public enum DisputeKind {
-    BOARD, PAYMENT, PRODUCT, CHAT
+    BOARD(1), CHAT(2), PAYMENT(3), PRODUCT(4);
+
+    private int num;
+
+    DisputeKind(int num) {
+        this.num = num;
+    }
+    public static DisputeReason getByNum(int num) {
+        return Arrays.stream(DisputeReason.values()).filter(r -> r.getNum() == num)
+                .findFirst().orElseThrow(() -> new ClientException(ErrorCode.ILLEGAL_EX_MESSAGE,
+                        "존재하지 않는 코드입니다."));
+    }
 }
