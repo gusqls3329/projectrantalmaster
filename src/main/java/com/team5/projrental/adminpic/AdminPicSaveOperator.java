fd9f23835550ca6d3631f8bee32d8f33f036817f
jo <yuyule20@naver.com>
2024-03-01 16:12:43 +0900

    2024-03-01 --by Hyunmin

--- src/main/java/com/team5/projrental/adminpic/AdminPicSaveOperator.java
+++ src/main/java/com/team5/projrental/adminpic/AdminPicSaveOperator.java
@@ -4,10 +4,18 @@ import com.team5.projrental.common.exception.checked.FileNotContainsDotException
 import com.team5.projrental.common.utils.MyFileUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +23,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class AdminPicSaveOperator {
 
     private final MyFileUtils fileUtils;
+    @Value("${file.package}")
+    private String basePath;
 
-    @PostMapping(value = "admin/pic-save", consumes = "multipart/form-data")
-    public String savePic(@RequestPart MultipartFile pic) {
+    @PostMapping(value = "admin/pic-save/{code}", consumes = "multipart/form-data")
+    public String savePic(@PathVariable String code, @RequestPart MultipartFile pic) {
+        if (!code.equals("even_last_6::electro")) return "FALSE";
+
+        File path = new File(basePath + "/admin/0");
+        File[] files = path.listFiles();
+        if (files != null && files.length > 0) {
+            return "EXISTS";
+        }
+
         try {
             fileUtils.savePic(pic, "admin", "0");
         } catch (FileNotContainsDotException e) {
