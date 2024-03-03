718de215facf0820b243c4341242ea33de9e5f22
devYule <privateDevYule@gmail.com>
2024-03-03 12:37:52 +0900

    2024-03-03 --by Hyunmin

--- src/main/java/com/team5/projrental/payment/kakao/KakaoPayController.java
+++ src/main/java/com/team5/projrental/payment/kakao/KakaoPayController.java
@@ -1,14 +1,18 @@
 package com.team5.projrental.payment.kakao;
 
+import com.team5.projrental.common.exception.ErrorMessage;
 import com.team5.projrental.payment.kakao.model.logic.PayApproveVo;
 import com.team5.projrental.payment.kakao.model.logic.PayReadyVo;
 import com.team5.projrental.payment.kakao.model.ready.PayInfoDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.DependentRequiredMap;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,10 +34,16 @@ public class KakaoPayController {
                           "nextRequestUrl: 다음 리다이렉트 경로, pg_token 은 따로 추출해서 다음 백엔드 서버로 요청시 제공<br>" +
                           "id: 결제 준비 요청 식별 id -> 다음 백엔드 서버로 요청시 제공<br><br>" +
                           "실패시: redirectUrl 에 .../payment/fail 이 담기고, id 가 null<br>" +
-                          "취소시: redirectUrl 에 .../payment/cancel 이 담기고, id 가 null<br> ")
+                          "취소시: redirectUrl 에 .../payment/cancel 이 담기고, id 가 null<br>" +
+                          "<br><br> " +
+                          "<strong>부연 설명</strong> <br>" +
+                          "redirect url 로 클라이언트 리다이렉트 -> 결제 진행 -> 결제 완료시 클라이언트 다시 우리 서버로 리다이렉트 됨 -> <br>" +
+                          "이때 쿼리파라미터로 pg_token 이 함께 제공됨 -> 이 pg_token 을 GET: " +
+                          "/api/pay/kakao/success/{id}?pg-token=<strong>여기!!</strong>" +
+                          " 에 제공하면 결제 완료됨<br>")
     @Schema(name = "kakaoPay")
     @PostMapping("/ready")
-    public PayReadyVo ready(@RequestBody PayInfoDto dto) {
+    public PayReadyVo ready(@RequestBody @Validated PayInfoDto dto) {
         return kakaoPayService.ready(dto);
     }
 
@@ -51,9 +61,10 @@ public class KakaoPayController {
                           "<br>" +
                           "실패시: 예외 발생")
     @Schema(name = "kakaoPay Approve")
+    @Validated
     @GetMapping("/success/{id}")
-    public PayApproveVo approve(@PathVariable Long id,
-                                @RequestParam("pg-token") String pgToken) {
+    public PayApproveVo approve(@PathVariable @Min(value = 1, message = ErrorMessage.ILLEGAL_RANGE_EX_MESSAGE) Long id,
+                                @RequestParam("pg-token") @NotBlank(message = ErrorMessage.CAN_NOT_BLANK_EX_MESSAGE) String pgToken) {
         return kakaoPayService.approve(pgToken, id);
     }
 
