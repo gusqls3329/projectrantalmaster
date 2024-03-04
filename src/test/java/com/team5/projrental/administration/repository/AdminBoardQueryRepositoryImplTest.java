3ed8c83ed831fe649ca99c5c884890ab6f0ff890
devYule <privateDevYule@gmail.com>
2024-03-04 14:26:31 +0900

    2024-03-04 --by Hyunmin

---
+++ src/test/java/com/team5/projrental/administration/repository/AdminBoardQueryRepositoryImplTest.java
@@ -0,0 +1,20 @@
+package com.team5.projrental.administration.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminBoardQueryRepositoryImplTest {
+
+    @Test
+    void findActivatedById() {
+    }
+
+    @Test
+    void findAllLimitPage() {
+    }
+
+    @Test
+    void totalCountByOptions() {
+    }
+}
