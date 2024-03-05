f17c4604c27f9618ac124435b0b6bdddf4f8f1b2
김현빈 <gusqls81017@gmail.com>
2024-03-05 17:07:14 +0900

    been first

--- src/main/java/com/team5/projrental/user/UserRepository.java
+++ src/main/java/com/team5/projrental/user/UserRepository.java
@@ -2,10 +2,13 @@ package com.team5.projrental.user;
 
 import com.team5.projrental.entities.User;
 import com.team5.projrental.entities.VerificationInfo;
+import com.team5.projrental.user.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> , UserQueryRepository{
 
+    Optional<User> findByUid(String id);
 }
--- src/main/java/com/team5/projrental/user/model/UserModel.java
+++ src/main/java/com/team5/projrental/user/model/UserModel.java
@@ -1,17 +1,33 @@
 package com.team5.projrental.user.model;
 
+import com.team5.projrental.entities.VerificationInfo;
+import com.team5.projrental.entities.embeddable.Address;
 import com.team5.projrental.entities.enums.Auth;
+import com.team5.projrental.entities.enums.ProvideType;
+import com.team5.projrental.entities.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
 
 @Data
+@Builder
 public class UserModel {
-    private Long iuser;
+    private Long id;
+    private Auth auth;
     private String uid;
     private String upw;
-    private String nm;
-    private String pic;
-    private String firebaseToken;
-    private String createdAt;
-    private String updatedAt;
-    private Auth Auth;
+    private String phone;
+    private String email;
+    private UserStatus status;
+    private String nick;
+    private String provideType;
+    private byte penalty;
+    private Address address;
+    private String storedPic;
+    private Double rating;
+    private String verification;
+
 }
