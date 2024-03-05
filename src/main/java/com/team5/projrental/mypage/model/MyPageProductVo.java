57575b6020bf8b132b1c725d9e6da81132fd4d5c
firstt <khi05040@naver.com>
2024-03-05 16:57:27 +0900

    board,mypage수정

--- src/main/java/com/team5/projrental/mypage/model/MyPageProductVo.java
+++ src/main/java/com/team5/projrental/mypage/model/MyPageProductVo.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 public class MyPageProductVo {
     private ProductMainCategory mainCategory;
     private ProductSubCategory subCategory;
+    private String storedPic;
 
     private Long iproduct;
     private String tilte;
